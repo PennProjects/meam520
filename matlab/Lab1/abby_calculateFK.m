afbed322c187190121f109d8d1b1afee771d4155
Abigail Haakyung Lee <haabibi623@gmail.com>
2020-09-19 21:00:18 -0400

    calcFK.m developed in our meeting

--- matlab/Lab1/abby_calculateFK.m
+++ matlab/Lab1/abby_calculateFK.m
@@ -33,16 +33,38 @@ L5 = 68;      % distance between joint 4 and center of gripper
                   0, sin(alpha), cos(alpha), d;
                   0, 0, 0, 1];
     end
+
+    %function [pos_matrix] = OToPos(o_init, TMatrix)
+        
+    %end
     
 jointPositions=zeros(6,3);
 T0e = eye(4,4);
-theta_1, theta_2, theta_3, theta_4, theta_5, dist_6 = q(1), q(2), q(3), q(4), q(5), q(6);
+theta_1, theta_2, theta_3, theta_4, theta_5 = q(1)+pi, q(2)+pi/2, q(3)+pi/2, q(4)+pi/2, q(5)+pi;
 
-T01 = DHParam(0, pi/2, L1, theta_1);
-T12 = DHParam(L2, 0, 0, theta_2);
-T23 = DHParam(0, 0, L3, theta_3);
-T34 = DHParam(0, pi/2, L4, theta_4);
-T4e = DHParam(0, 0, L5, theta_5);
+T10 = DHParam(0, pi/2, L1, theta_1);
+T21 = DHParam(L2, 0, 0, theta_2);
+T32 = DHParam(L3, 0, 0, theta_3);
+T43 = DHParam(0, pi/2, 0, theta_4);
+Te4 = DHParam(0, 0, L4+L5, theta_5);
+
+% origins of the 6
+O0_init = [0 0 0 1]
+O1_init = [0 0 L1 1]
+O2_init = [0 0 L1+L2 1]
+O3_init = [L3 0 L1+L2 1]
+O4_init = [L3 0 L1+L2 1]
+Oe_init = [L3+L4+L5 0 L1+L2 1]
+
+% joint pos of 6
+O1 = T10 * O1_init
+O2 = T10 * T21 * O2_init
+O3 = T10 * T21 * T32 * O3_init
+O4 = T10 * T21 * T32 * T43 * O4_init
+Oe = T10 * T21 * T32 * T43 * Te4 * Oe_init
+
+% Have to get the O1 to joints 
+% form 6 joints [1x3] to [6x3]
 
 T0e = T01 * T12 * T23 * T34 * T4e;
 
