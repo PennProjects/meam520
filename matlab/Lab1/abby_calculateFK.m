1bafb4c2aaf87365b2901081570f2da1f048762a
Abigail Haakyung Lee <haabibi623@gmail.com>
2020-09-21 15:11:23 -0400

    transformation done, joint needs to be fixed little

--- matlab/Lab1/abby_calculateFK.m
+++ matlab/Lab1/abby_calculateFK.m
@@ -34,13 +34,11 @@ L5 = 68;      % distance between joint 4 and center of gripper
                   0, 0, 0, 1];
     end
 
-    %function [pos_matrix] = OToPos(o_init, TMatrix)
-        
-    %end
-    
-jointPositions=zeros(6,3);
-T0e = eye(4,4);
-theta_1, theta_2, theta_3, theta_4, theta_5 = q(1)+pi, q(2)+pi/2, q(3)+pi/2, q(4)+pi/2, q(5)+pi;
+theta_1 = q(1)+pi;
+theta_2 = q(2)+pi/2;
+theta_3 = q(3)+pi/2;
+theta_4 = q(4)+pi/2;
+theta_5 = q(5)+pi;
 
 T10 = DHParam(0, pi/2, L1, theta_1);
 T21 = DHParam(L2, 0, 0, theta_2);
@@ -49,24 +47,22 @@ T43 = DHParam(0, pi/2, 0, theta_4);
 Te4 = DHParam(0, 0, L4+L5, theta_5);
 
 % origins of the 6
-O0_init = [0 0 0 1]
-O1_init = [0 0 L1 1]
-O2_init = [0 0 L1+L2 1]
-O3_init = [L3 0 L1+L2 1]
-O4_init = [L3 0 L1+L2 1]
-Oe_init = [L3+L4+L5 0 L1+L2 1]
+%O0_init = [0 0 0 1];
+%O1_init = [0 0 L1 1];
+%O2_init = [0 0 L1+L2 1];
+%O3_init = [L3 0 L1+L2 1];
+%O4_init = [L3 0 L1+L2 1];
+%Oe_init = [L3+L4+L5 0 L1+L2 1];
 
 % joint pos of 6
-O1 = T10 * O1_init
-O2 = T10 * T21 * O2_init
-O3 = T10 * T21 * T32 * O3_init
-O4 = T10 * T21 * T32 * T43 * O4_init
-Oe = T10 * T21 * T32 * T43 * Te4 * Oe_init
+O1 = T10;
+O2 = T10 * T21 ;
+O3 = T10 * T21 * T32;
+O4 = T10 * T21 * T32 * T43;
+Oe = T10 * T21 * T32 * T43 * Te4;
 
-% Have to get the O1 to joints 
-% form 6 joints [1x3] to [6x3]
-
-T0e = T01 * T12 * T23 * T34 * T4e;
+T0e = T10 * T21 * T32 * T43 * Te4;
+jointPositions = [ 0 0 0; O1([13 14 15]); O2([13 14 15]); O3([13 14 15]); O4([13 14 15]); Oe([13 14 15])];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
