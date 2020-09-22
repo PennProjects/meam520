8541994e2504aa29c3ddd86c100422a176aeec1e
Jal Panchal <jlpnchl@gmail.com>
2020-09-22 18:04:58 -0400

    added 3d boundaries

--- matlab/Lab1/Jal_computeWorkspace.m
+++ matlab/Lab1/Jal_computeWorkspace.m
@@ -6,7 +6,7 @@ addpath('../Core') % references ROS interface and arm controller files you'll ne
 
 gripper_positions = [];
 
-for th_1  = -1.4 :0.5 :1.4
+for th_1  = -1.4 :0.2 :1.4
     for th_2 = -1.2 :0.5 : 1.4
         for th_3 = -1.8 : 0.5: 1.7
             for th_4 = -1.9 : 0.5 : 1.7
@@ -34,6 +34,7 @@ z = gripper_positions(:,3)
 b_x = boundary(y,z)
 b_y = boundary(x,z)
 b_z = boundary(x,y)
+b_3d = boundary(gripper_positions)
 
 
 plot3(x,y,z, 'o', 'Color','#FF0000')
@@ -53,6 +54,11 @@ plot3(1.1*max(x)*ones(size(x)),y,z, '-', 'Color', '#add8e6')
 plot3(1.1*max(x)*ones(size(y(b_x))), y(b_x), z(b_x), '-', 'Color', '#FF0000')
 plot3(x, y, 1.1*min(z)*ones(size(z)), '-', 'Color', '#add8e6')
 plot3(x(b_z), y(b_z),1.1*min(z)*ones(size(x(b_z))) , '-', 'Color', '#FF0000')
+
+trisurf(b_3d,x,y,z, 'Facecolor', 'r', 'FaceAlpha', '0.1')
+
+
+
 
 
  
