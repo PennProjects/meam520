a7baa8d93f6c060ae3ed8b7dd76585db94008c2e
Jal Panchal <jlpnchl@gmail.com>
2020-09-22 17:30:43 -0400

    added 2d projections

--- matlab/Lab1/Jal_computeWorkspace.m
+++ matlab/Lab1/Jal_computeWorkspace.m
@@ -1,28 +1,52 @@
+clear
+clc
+
 addpath('../Core') % references ROS interface and arm controller files you'll need for every lab
 
 
-gripper = [];
-q_concat = [];
+gripper_positions = [];
 
-for th_1  = -1.4 :0.1 :1.4
-    for th_2 = -1.2 :0.1 : 1.4
-        for th_3 = -1.8 : 0.1: 1.7
-            for th_4 = -1.9 : 0.1 : 1.7
-                for th_5 = -2 : 0.1 : 1.5
+for th_1  = -1.4 :0.5 :1.4
+    for th_2 = -1.2 :0.5 : 1.4
+        for th_3 = -1.8 : 0.5: 1.7
+            for th_4 = -1.9 : 0.5 : 1.7
+                for th_5 = -2 : 0.5 : 1.5
                         
                         q = [th_1, th_2, th_3, th_4, th_5, 0];
-                        [jointPositions, T0e] = abby_calculateFK(q);
-                        gripper = [gripper; T0e([13,14,15])];
-%                         q_concat = [q_concat;q];
-%                             disp(T0e);
-                        
-                    
+                        [jointPositions, T0e] = calculateFK(q);
+                        gripper_positions = [gripper_positions; T0e([13,14,15])];
+                                         
                 end
-                endx
+            end
         end
     end                
-end;
+end
 
 
 % disp(gripper)
-scatter3(gripper(:,1), gripper(:,2), gripper(:,3))
+% scatter3(gripper_positions(:,1), gripper_positions(:,2), gripper_positions(:,3))
+
+% plot the workspace
+x = gripper_positions(:,1)
+y = gripper_positions(:,2)
+z = gripper_positions(:,3)
+
+plot3(x,y,z, '.')
+grid on
+xlabel('Xo')
+ylabel('Yo')
+zlabel('Zo')
+xlim([1.1*min(x),1.1*max(x)])
+ylim([1.1*min(y),1.1*max(y)])
+zlim([1.1*min(z),1.1*max(z)])
+
+hold on
+plot3(x, 1.1*max(y)*ones(size(y)),z, '-');
+plot3(1.1*max(x)*ones(size(x)),y,z, '-');
+plot3(x, y, 1.1*min(z)*ones(size(z)), '-');
+
+
+
+
+
+
