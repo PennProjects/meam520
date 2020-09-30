a88ff2103b28e860584822b871d75998718d7c0f
Jal Panchal <jlpnchl@gmail.com>
2020-09-30 01:00:08 -0400

    Tsting geometric ik for th3

--- matlab/Lab2/wrist_positiontest.m
+++ matlab/Lab2/wrist_positiontest.m
@@ -26,16 +26,29 @@ w = H03([13 14 15])
 
 a1 = [76.2, 146.05, 187.325];
 % q1 = [0,0,0,0,0,0];
-q2 = [-1.4, 0, pi/4, 0,1.5,0];
-q3 = [pi/2, 0, pi/4,0,pi/2,0];
-w_1= calc_wrist_pos(a1,q3)
+q2 = [-1, 0, 0.2, 0,1.5,0];
+% q3 = [pi/2, 0, pi/4,0,pi/2,0];
+
+q = q2;
+w_1= calc_wrist_pos(a1,q)
+
+q_test = angle_ik(a1, w_1)
+q
 
 
 function [w_value] = calc_wrist_pos(a, q)
 
     w_value =  [a(2)*cos(q(1))*sin(q(2)) - a(3)*cos(q(1))*sin(q(2))*sin(q(3)) + a(3)*cos(q(1))*cos(q(2))*cos(q(3));
-                a(2)*sin(q(1))*sin(q(2)) - a(3)*sin(q(1))*sin(q(2))*sin(q(3)) + a(3)*cos(q(2))*cos(q(3))*sin(q(1));
+                a(2)*sin(q(1))*sin(q(2)) - a(3)*sin(q(1))*sin(q(2))*sin(q(3)) + a(3)*sin(q(1))*cos(q(2))*cos(q(3));
                 a(1) + a(2)*cos(q(2)) - a(3)*cos(q(2))*sin(q(3)) - a(3)*cos(q(3))*sin(q(2))];
 
+end
+
+
+function [q_value] = angle_ik(a, pos)
+    
+    q_value = [atan2(pos(2),pos(1)),
+               0,
+               asin((a(2)^2+ a(3)^2 -(((pos(1)^2 + pos(2)^2)/pos(2)^2)*(pos(2)^2+(a(1)-pos(3))^2)))/(2*a(2)*a(3)))];
 end
         
