c7835deae4f5c8544e4567c5e642598e4de8c7bf
Abigail Haakyung Lee <haabibi623@gmail.com>
2020-10-08 00:49:17 -0400

    check point is on the planar

--- matlab/Lab2/abby_methods.m
+++ matlab/Lab2/abby_methods.m
@@ -12,11 +12,13 @@ c1 = cos(q1); s1 = sin(q1);
 c2 = cos(q2); s2 = sin(q2);
 c3 = cos(q3); s3 = sin(q3); 
 
-H03 = [ c1*c2*c3 - c1*s2*s3, - c1*c2*s3 - c1*c3*s2, -s1, a2*c1*s2 - a3*c1*s2*s3 + a3*c1*c2*c3;
-        c2*c3*s1 - s1*s2*s3, - c2*s1*s3 - c3*s1*s2,  c1,    a2*s1*s2 - a3*s1*s2*s3 + a3*c2*c3*s1;
-        - c2*s3 - c3*s2,         s2*s3 - c2*c3,         0,     d1 + a2*c2 - a3*c2*s3 - a3*c3*s2;
-                   0,                     0,            0,                                    1];
-
+% H03 = [ c1*c2*c3 - c1*s2*s3, - c1*c2*s3 - c1*c3*s2, -s1, a2*c1*s2 - a3*c1*s2*s3 + a3*c1*c2*c3;
+%         c2*c3*s1 - s1*s2*s3, - c2*s1*s3 - c3*s1*s2,  c1,    a2*s1*s2 - a3*s1*s2*s3 + a3*c2*c3*s1;
+%         - c2*s3 - c3*s2,         s2*s3 - c2*c3,         0,     d1 + a2*c2 - a3*c2*s3 - a3*c3*s2;
+%                    0,                     0,            0,                                    1];
+% O03_x = H03(1,4);
+% O03_y = H03(2,4);
+% O03_z = H03(3,4);
 
 
 origin = [0 0 0];
@@ -36,7 +38,7 @@ function [w_value] = calc_wrist_pos(a, q)
 end
 
 function [ a, b, c, d ] = get_plane_eq( p1, p2, p3 )
-do 
+
   a = ( p2(2) - p1(2) ) * ( p3(3) - p1(3) ) ...
     - ( p2(3) - p1(3) ) * ( p3(2) - p1(2) );
 
@@ -50,4 +52,14 @@ do
 
   return
 end
+
+function [onPlane] = check_on_plane( ori, a, b, c, d)
+    if a*ori(1) +b*ori(2)+c*ori(3)+d == 0
+        onPlane = 1;
+    else
+        onPlane = 0;
+    end
+    return
+end
+
 
