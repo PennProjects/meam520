fbd19d5005de65fa81a9405c03c2d50c81ab088c
Jal Panchal <jlpnchl@gmail.com>
2020-10-08 23:33:53 -0400

    found proj on plane

--- matlab/Lab2/jal_plane_normals.m
+++ matlab/Lab2/jal_plane_normals.m
@@ -12,10 +12,14 @@ d5 = 34;                        % Distance between joint 4 and end effector
 % T0e = [[   0.019,    0.969,    0.245,   47.046];[   0.917,   -0.115,    0.382,   73.269];[   0.398 ,   0.217,   -0.891,  100.547];[   0.,       0. ,      0.,       1.]];
 
 % Target 2
-T0e = [[  -0.993,   -0.,       0.119,  -96.936];[   0.,      -1.,      -0.,       0.   ];[   0.119,    0.,       0.993,  401.229];[   0. ,      0.  ,     0.  ,     1.   ]];
+% T0e = [[  -0.993,   -0.,       0.119,  -96.936];[   0.,      -1.,      -0.,       0.   ];[   0.119,    0.,       0.993,  401.229];[   0. ,      0.  ,     0.  ,     1.   ]];
 
 % Target 3
-% T0e =[ [-0.3409003, -0.1074855,  0.9339346, 282.96];[0.7842780, -0.5802868,  0.2194888, -48.302];[0.5183581,  0.8072881,  0.2821184, 235.071 ]; [0,0,0,1]];
+T0e =[ [-0.3409003, -0.1074855,  0.9339346, 282.96];[0.7842780, -0.5802868,  0.2194888, -48.302];[0.5183581,  0.8072881,  0.2821184, 235.071 ]; [0,0,0,1]];
+
+% Target 4lab2  
+% T0e =[[  0.5054096, -0.8370580, -0.2095115, -45];[-0.0305796,  0.2252773, -0.9738147,-300];[0.8623375,  0.4985821,  0.0882604, 63 ];[0,0,0,1]];
+
 
 
 % Decomposing T0e
@@ -36,21 +40,30 @@ z = T0e(3,4);
 x_c = x - (d4+d5) * r13; 
 y_c = y - (d4+d5) * r23;
 z_c = z - (d4+d5) * r33;
-o_c = [x_c; y_c; z_c] ;
+o_c = [x_c y_c z_c] ;
 
 
 
 p1 = [0 0 0];
 p2 = [0 0 50];
-p3 = transpose(o_c);
+p3 = o_c;
 
-normal = cross(p1-p2,p1-p3);
-normal = 1*normal/norm(normal);
+normal_vec = cross(p1-p2,p1-p3);
+normal_vec = 1*normal_vec/norm(normal_vec);
+
+normal_line = [o_c; (o_c+normal_vec*10)];
 
 
 e_desired = [x,y,z];
+e_norm = e_desired/norm(e_desired);
+e_from_wrist = e_desired-o_c;
+e_proj_on_normal = dot(e_from_wrist,normal_vec)*normal_vec;
+e_possible_from_wrist =  e_from_wrist-e_proj_on_normal;
+e_possible = e_possible_from_wrist+o_c;
+e_pos_norm = e_possible/norm(e_possible);
 
-x = dot(normal, e_desired)
+not_in_plane = round(dot(normal_vec, e_norm),3)
+is_in_plane = round(dot(normal_vec, e_pos_norm),3)
 
 
 %method 2
@@ -83,6 +96,9 @@ plot3(p1(1),p1(2),p1(3), '.', 'MarkerSize',50,'color', 'b')
 plot3(p2(1),p2(2),p2(3), '.', 'MarkerSize',50, 'color', 'b')
 plot3(p3(1),p3(2),p3(3), '.', 'MarkerSize',50, 'color', 'b')
 plot3(e_desired(1),e_desired(2),e_desired(3),'.', 'MarkerSize',50,'color', 'r')
+plot3(normal_line(:,1), normal_line(:,2), normal_line(:,3), 'LineWidth', 5,'color', '#000000')
+plot3(e_possible(1),e_possible(2), e_possible(3), '.', 'MarkerSize',50,'color', 'g')
+
 
 xlabel('Xo', 'FontSize', 20, 'FontWeight', 'bold');
 ylabel('Yo', 'FontSize', 20, 'FontWeight', 'bold');
