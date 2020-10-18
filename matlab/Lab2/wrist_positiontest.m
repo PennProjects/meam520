1f8865cb1daf0a4559514811d827342bf6e3ec0f
Jal Panchal <jlpnchl@gmail.com>
2020-10-18 00:21:45 -0400

    finding gaps in theta2,3 ik cover

--- matlab/Lab2/wrist_positiontest.m
+++ matlab/Lab2/wrist_positiontest.m
@@ -80,10 +80,9 @@ bl = [];
 % end 
 
 
-
 for th1 = -1.4 : 0.2 :1.4
-   for th2 = -1.2 :0.5 : 1.4
-        for th3 = -1.8 : 0.3 : 1.7
+   for th2 = -1.2 :0.3 : 1.4
+        for th3 = -1.8 : 0.2 : 1.7
              q1 = [th1,th2,th3,0,0,0];
     
 
@@ -94,16 +93,31 @@ for th1 = -1.4 : 0.2 :1.4
                 q_test_2 = angle_ik(a1, w_1, 2);
                 q_test_3 = angle_ik(a1, w_1, 3);
                 q_test_4 = angle_ik(a1, w_1, 4);
-                q_test_con = [q_test_con;q(:,1:3),bl,q_test_1,q_test_2, q_test_3, q_test_4];
+                q_test_5 = angle_ik(a1, w_1, 5);
+                q_test_6 = angle_ik(a1, w_1, 6);
+                q_test_7 = angle_ik(a1, w_1, 7);
+                q_test_8 = angle_ik(a1, w_1, 8);
+                q_test_con = [q_test_con;q(:,1:3),bl,q_test_1,q_test_2, q_test_3, q_test_4, q_test_5, q_test_6, q_test_7,q_test_8];
+                
+                
         end
    end
 end
 
 q_test_con = round(q_test_con, 5);
 
-q_test_con(:,16) =  (q_test_con(:,1)==q_test_con(:,4))+ (q_test_con(:,1)==q_test_con(:,7)) + (q_test_con(:,1)==q_test_con(:,10)) + (q_test_con(:,1)==q_test_con(:,13))	;
-q_test_con(:,17) = (q_test_con(:,2)==q_test_con(:,5)) + (q_test_con(:,2)==q_test_con(:,8)) + (q_test_con(:,2)==q_test_con(:,11)) + (q_test_con(:,2)==q_test_con(:,14)) ;
-q_test_con(:,18) = (q_test_con(:,3)==q_test_con(:,6)) + (q_test_con(:,3)==q_test_con(:,9)) + (q_test_con(:,3)==q_test_con(:,12)) + (q_test_con(:,3)==q_test_con(:,15));
+q_test_con(:,28) =  ((q_test_con(:,1)==q_test_con(:,4))+ (q_test_con(:,1)==q_test_con(:,7)) + (q_test_con(:,1)==q_test_con(:,10)) + (q_test_con(:,1)==q_test_con(:,13)) + (q_test_con(:,1)==q_test_con(:,16)) + (q_test_con(:,1)==q_test_con(:,19)) + (q_test_con(:,1)==q_test_con(:,22)) + (q_test_con(:,1)==q_test_con(:,25)));
+q_test_con(:,29) = ((q_test_con(:,2)==q_test_con(:,5)) + (q_test_con(:,2)==q_test_con(:,8)) + (q_test_con(:,2)==q_test_con(:,11)) + (q_test_con(:,2)==q_test_con(:,14)) + (q_test_con(:,2)==q_test_con(:,17)) + (q_test_con(:,2)==q_test_con(:,20)) + (q_test_con(:,2)==q_test_con(:,23)) + (q_test_con(:,2)==q_test_con(:,26))) ;
+q_test_con(:,30) = ((q_test_con(:,3)==q_test_con(:,6)) + (q_test_con(:,3)==q_test_con(:,9)) + (q_test_con(:,3)==q_test_con(:,12)) + (q_test_con(:,3)==q_test_con(:,15)) + (q_test_con(:,3)==q_test_con(:,18)) + (q_test_con(:,3)==q_test_con(:,21)) + (q_test_con(:,3)==q_test_con(:,24)) + (q_test_con(:,3)==q_test_con(:,27)));
+
+q_notfound = [sum(q_test_con(:,28)==0),sum(q_test_con(:,29)==0), sum(q_test_con(:,30)==0), size(q_test_con, 1)];
+
+q_theta2 = [];
+q_theta1 = [];
+for i = 1:9
+    q_theta2 = [q_theta2, q_test_con(:,-1+3*i)];
+    q_theta1 = [q_theta1, q_test_con(:,3*i)];
+end
 
 
 %single value test1
@@ -122,53 +136,85 @@ end
 
 
 function [q_value] = angle_ik(a, pos, option)
+x_c = pos(1);
+y_c = pos(2);
+z_c = pos(3);
 
-    if option ==1
-        theta1 = atan2(pos(2),pos(1));
+d1 = a(1);
+a2 = a(2);
+a3 = a(3);
+
+lowerLim = [-1.4, -1.2, -1.8, -1.9, -2.0, -15]; % Lower joint limits in radians (grip in mm (negative closes more firmly))
+upperLim = [ 1.4,  1.4,  1.7,  1.7,  1.5,  30]; % Upper joint limits in radians (grip in mm)
+
+    %Calculating q1 
+    q1 = atan2(y_c,x_c);
+    % q1 = atan2(abs(y_c),abs(x_c));
+    if q1==pi ||q1 == -pi
+      q1=0; % To remove -pi and pi from answers
+    end
+   
+ q1_a = [];
+ for i=1: 8
+    if i ==1
+        q1_a(i) = q1;
+    elseif i ==2;
+         q1_a(i) = pi+q1;
+%     elseif i ==3;
+%          q1_a(i) = pi-q1;
+%     elseif i ==4;
+%          q1_a(i) = -q1;
+    elseif i ==5;
+         q1_a(i) = q1-pi;         
     else
-        theta1 = atan2(pos(2),pos(1))-pi;
-    end
-    
-    if theta1 == pi || theta1 ==-pi
-        theta1 =0;
-    end
-    
-    gamma = (a(2)^2+ a(3)^2 -(pos(1)^2 + pos(2)^2)-((pos(3)-a(1))^2))/(2*a(2)*a(3));
-%       omega = ((a(2)^2+ a(3)^2 -(pos(1)^2 + pos(2)^2)-((pos(3)-a(1))^2))/(2*a(2)*a(3)));
-%       gamma = atan2(sqrt(1-omega^2), omega);
-    
-      
-      if gamma <0  
-        theta3 = (acos(gamma)-3*pi/2);
-      else
-          if option==1 || option==2
-            theta3 = (pi/2-acos(gamma));
-          else
-            theta3 = (acos(gamma)-3*pi/2);
-          end        
-      end
+        q1_a(i) = q1;
+    end    
+end
 
-    alpha = atan2((pos(3)-a(1)), (sqrt(pos(1)^2 + pos(2)^2)));
-    beta = atan2((a(3)*cos(theta3)),(a(2)-a(3)*sin(theta3)));
 
-    if option ==1 
-        theta2 = pi/2-alpha-beta;
-    elseif option ==2
-        theta2 = -pi/2+alpha-beta;
-    elseif option ==3
-        theta2 = pi/2-alpha+beta;        
+%Calculating q3
+gamma = acos((a2^2 + a3^2 - x_c^2 - y_c^2 - (z_c-d1)^2) / (2*a2*a3));
+
+%2 values for elbow up and elbow dowm orientation
+q3_a(1) = pi/2-gamma;
+q3_a(2) = gamma-3*pi/2 ;
+
+
+
+%Calculating q2
+%Here we calculate 8 values 
+%2 for elbow-up and elbow-down for +ve q2 and 2 for elbow-up and elbow-down for -ve q2 
+%the 4 values are then calculated for each value of q3 
+% We later discard the incorrect/out of range values
+q2_a(1) = pi/2-atan2((z_c-d1), sqrt(x_c^2+y_c^2))-atan2(a3*cos(q3_a(1)),(a2-a3*sin(q3_a(1))));
+q2_a(2) = pi/2-atan2((z_c-d1), sqrt(x_c^2+y_c^2))+atan2(a3*cos(q3_a(1)),(a2-a3*sin(q3_a(1))));
+q2_a(3) = -pi/2+atan2((z_c-d1), sqrt(x_c^2+y_c^2))-atan2(a3*cos(q3_a(1)),(a2-a3*sin(q3_a(1))));
+q2_a(4) = -pi/2+atan2((z_c-d1), sqrt(x_c^2+y_c^2))+atan2(a3*cos(q3_a(1)),(a2-a3*sin(q3_a(1))));
+
+% % q2_a(5) = pi/2-atan2((z_c-d1), sqrt(x_c^2+y_c^2))-atan2(a3*cos(q3_a(2)),(a2-a3*sin(q3_a(2))));
+% % q2_a(6) = pi/2-atan2((z_c-d1), sqrt(x_c^2+y_c^2))+atan2(a3*cos(q3_a(2)),(a2-a3*sin(q3_a(2))));
+% % q2_a(7) = -pi/2+atan2((z_c-d1), sqrt(x_c^2+y_c^2))-atan2(a3*cos(q3_a(2)),(a2-a3*sin(q3_a(2))));
+% % q2_a(8) = -pi/2+atan2((z_c-d1), sqrt(x_c^2+y_c^2))+atan2(a3*cos(q3_a(2)),(a2-a3*sin(q3_a(2))));
+
+q2_a(5) = q2_a(1);
+q2_a(6) = q2_a(1);
+q2_a(7) = q2_a(1);
+q2_a(8) = q2_a(1);
+
+
+%Constructing q3 vector to match the size of q2 vector
+q3_b = [];
+for i=1: 8
+    if i == 1 || i ==4
+        q3_b(i) = q3_a(1);
+    elseif i ==2 || i == 3
+        q3_b(i) = q3_a(2);
     else
-        theta2 = -pi/2+alpha+beta;
-    end
-
-
-    theta3_cy = -pi/2 + acos((-a(2)^2 - a(3)^2 +(pos(1)^2 + pos(2)^2)+((a(1)-pos(3))^2))/(2*a(2)*a(3)));
-%     theta3 = theta3_cy;
-    
-    theta2_cy = pi/2 -atan2((pos(3)-a(1)),(sqrt(pos(1)^2 + pos(2)^2))) + atan2((a(3)*sin(-pi/2-theta3)),(a(2)+a(3)*cos(-pi/2-theta3)));
-%     theta2 = theta2_cy;
+        q3_b(i) = 0;
+    end    
+end
 
     
-    q_value = [theta1 theta2 theta3];
+    q_value = [q1_a(option), q2_a(option), q3_b(option)];
 end
         
