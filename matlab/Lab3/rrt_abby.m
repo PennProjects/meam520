b7851ca3310059f0991e9ebed314ba5ddcd49cab
Abigail Haakyung Lee <haabibi623@gmail.com>
2020-10-21 22:05:10 -0400

    abby code mixed up -sorry!

--- matlab/Lab3/rrt_abby.m
+++ matlab/Lab3/rrt_abby.m
@@ -1,4 +1,4 @@
-function [path] = rrt(map, start, goal)
+function [path] = rrt_abby(map, start, goal)
 % RRT Find the shortest path from start to goal.
 %   PATH = rrt(map, start, goal) returns an mx6 matrix, where each row
 %   consists of the configuration of the Lynx at a point on the path. The
@@ -24,38 +24,92 @@ robot.lowerLim = [-1.4000 -1.2000 -1.8000 -1.9000 -2 -15];
 robot.upperLim = [1.4000 1.4000 1.7000 1.7000 1.5000 30];
 robot.d4 = 34;
 
-q_init = robot.lowerLim;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                  Algortihm Starts Here             %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 total_iterations = 3;
 
-for i = 1:total_iterations
+tree(1).coord = start; 
+tree(1).parent = 0;
+
+for i = 2:total_iterations
     % assume we have a new point
-    q_new = [q_init(1)*rand(1) q_init(2)*rand(1) q_init(3)*rand(1) start(4) start(5) start(6)];
+    q_new = [robot.lowerLim(1)*rand(1) robot.lowerLim(2)*rand(1) robot.lowerLim(3)*rand(1) start(4) start(5) start(6)];
     
+    % check whether this random point collides with obstacle
     isCollided = checkCollision(q_new); 
       
     if isCollided
         continue;
     end
     
-    path = [path; q_new];
-    q_init = q_new;
+    % find q_a that is closest node in T_start
+    closest_dist = Inf;
+    for j  = 1:i
+        tmp_dist =  sqrt((curr_q(1) - tree(j).q(1))^2 + (curr_q(1) - tree(j).q(2))^2 + (curr_q(1) - tree(j).q(3))^2);
+        if closest_dist < tmp_dist
+            closest_dist = tmp_dist; 
+            closest_node = j;
+            q_a = tree(j).q;
+        end
+    end
+    
+    % find q_a' that is interpolated points between q_a and q_new
+    q_a_dash = interpolate(q_new, q_a);
+    
+    % or move towards that q_new position by some delta and set it q_a_dash
+    
+    
+    % if not collide(q, q_a') -> add (q, q_a') to T_start
+    
+    
+    if isCollided == 0
+        tree(i).coord = q_new;
+        tree(i).parent = closest_node;
+    end
+    
+    reachedGoal = checkReachedGoal(q_new, goal);
+    if reachedGoal
+        path = [];
+        tree_node_num = i;
+        while(tree_node_num ~= -1)
+            path = [path; tree(tree_node_num)];
+            tree_node_num = tree_node(tree_node_num).parent;
+        end
+        break
+    end
     
 end
 
-path = [pi/4 0 0 0 0 0; -pi/4 0 0 0 0 0];
-% path = [pi/4 pi/4 pi/4 pi/4 pi/4 pi/4; 
-%         -pi/4 -pi/4 pi/4 pi/4 pi/4 pi/4;]
-path = [start; path];
-path = [path; goal]; 
+% path = [pi/4 0 0 0 0 0; -pi/4 0 0 0 0 0];
+% % path = [pi/4 pi/4 pi/4 pi/4 pi/4 pi/4; 
+% %         -pi/4 -pi/4 pi/4 pi/4 pi/4 pi/4;]
+% path = [start; path];
+% path = [path; goal]; 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                  Algortihm Ends Here               %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function [reachedGoal] = checkReachedGoal(q_new, goal)
+        if q_new(1) == goal(1) && q_new(2) == goal(2) && q_new(3) == goal(3) 
+            reachedGoal = 1;
+        else
+            reachedGoal = 0;
+        end
+    end
+    
+    function [q_a_dash] = interpolate( q_new, q_a)
+        q_a_dash = [];
+        for i=1:5
+            q1 = ;
+            q2 = ;
+            q3 = ;
+            q_a_dash = [q_a_dash; q1 q2 q3];
+        end
+            
+    end
 
     function [isCollided] = checkCollision(q_new)
         % For each obstacle in the space
@@ -73,6 +127,7 @@ path = [path; goal];
             isCollided = detectCollision(linePt1, linePt2,currObstacle);
             
         end
+
     end
 
 
