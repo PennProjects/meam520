bc2a9fc97b96bba3490ffd3d65590cf4cf489698
Jal Panchal <jlpnchl@gmail.com>
2020-10-22 18:31:24 -0400

    simple rrt working

---
+++ matlab/Lab3/TestPath_Sim_mod1022.m
@@ -0,0 +1,66 @@
+%% Setup
+clear all
+close all
+addpath('maps')
+addpath('../Core')
+
+%% Simulation Parameters
+
+start = [0,pi/4,0,0,0,0];
+goal = [0,-pi/4,0,0,0,0];
+
+map = loadmap('map2.txt');
+
+% Find collision-free path using RRT to get list of waypoints
+[path] = jal_rrt(map, start, goal);
+
+% [path] = astar(map, start, goal);
+
+%start ROS
+lynx = ArmController();
+pause(1) % wait for setup
+collision = false;
+lynx.set_pos([0 0 0 0 0 0])
+pause(10)
+path
+% iterate over target waypoints
+for target_index = 1:length(path(:,1))
+    q = path(target_index, :);
+    disp("Goal:")
+    disp(q)
+    lynx.set_pos(q)
+    reached_target = false;
+    e = 0.001;
+    % Define relevant variables here:
+    count = 0;
+    while ~reached_target
+        % Check if robot is collided then wait
+%         collision = collision || lynx.is_collided();
+        pause(0.1)
+        collision = collision | lynx.is_collided();
+
+        % Add Student code here to decide if controller should send next
+        % target or continue to wait. Do NOT add additional pauses to control
+        % loop. You will likely want to use lynx.get_state() to decide when to
+        % move to the next target.
+        count = count + 1;
+        if count > 10
+            reached_target = true;
+            count = 0;
+        end
+
+        % End of student code
+    end
+    % End control loop
+
+    disp("Current Configuration:");
+    [pos, vel] = lynx.get_state();
+    disp(pos);
+end
+if collision
+    disp("Robot collided during move")
+else
+    disp("No collision detected")
+end
+
+lynx.stop() % Shut down ROS interface
