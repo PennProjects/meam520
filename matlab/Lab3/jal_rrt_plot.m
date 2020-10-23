91f3ec3492e54df09281cbfff8b623dbebca0a60
Jal Panchal <jlpnchl@gmail.com>
2020-10-23 18:31:33 -0400

    testing 2 trees

--- matlab/Lab3/jal_rrt_plot.m
+++ matlab/Lab3/jal_rrt_plot.m
@@ -33,12 +33,24 @@ total_iterations = 3;
 
 tree(1).coord = start; 
 tree(1).parent = -1;
+
+%%%%%%%%%
+tree_end(1).coord = goal;
+tree_end(1).parent = -1; 
+%%%%%%%%%
+
+
 epsilon = 0.01;
 reachedGoal = checkReachedGoal(start, goal, epsilon);
 alpha = 1000;
 
 
 while reachedGoal == 0
+    
+    %%%%
+    num_added_tree = 0; 
+    %%%%
+    
     % assume we have a new point
     temp_lowerlimit = min(start, goal)-epsilon;
     temp_upperlimit = max(start,goal)+epsilon;
@@ -93,24 +105,73 @@ while reachedGoal == 0
     
     [isCollided2] = checkCollision(between_start_points, between_end_points, map); 
     
-    if isCollided 
-        continue
-    elseif isCollided2
-        continue
-    else
+%     if isCollided 
+%         continue
+%     elseif isCollided2
+%         continue
+%     else
+    if any(isCollided)==0 && any(isCollided2)==0
         tree(size_tree+1).parent = min_dist_idx;
         tree(size_tree+1).coord = q_new;
+        %%%
+        num_added_tree = num_added_tree + 1;
+        size_tree = size(tree, 2);
+        %%%
     end
     
+    %%%%%%% checking for tree_end 
+    dist_array_end = [];
+    size_tree_end = size(tree_end, 2);
+    for idx = 1:size_tree_end
+        dist_array_end = [dist_array_end; sqrt(sum((q_new - tree(idx).coord).^2))];
+    end
+    [min_dist_end, min_dist_idx_end] = min(dist_array_end);
     
+    closest_q_end = tree_end(min_dist_idx_end).coord;
+    
+    [start_points_end, end_points_end] = collision_check_points(q_new, closest_q_end); 
+    [isCollided_end] = checkCollision(start_points_end, end_points_end, map); 
+    
+    % check collision for points inbetween closest_q, q_new 
+    % (interpolation in configuration space) 
+    [between_start_end, between_end_end] = generate_points_inbetween(closest_q_end, q_new, alpha);
+    
+    between_start_points_end = [];
+    between_end_points_end = []; 
+    for num = 1:size(between_start_end, 1)
+        [between_start_point_end, between_end_point_end] = collision_check_points(between_start_end(num, :), between_end_end(num, :)); 
+        between_start_points_end = [between_start_points_end; between_start_point_end]; 
+        between_end_points_end = [between_end_points_end; between_end_point_end];
+    end
+    
+    [isCollided2_end] = checkCollision(between_start_points_end, between_end_points_end, map); 
+    
+%     if isCollided_end 
+%         continue
+%     elseif isCollided2_end
+%         continue
+%     else
+    if any(isCollided_end) ==0  && any(isCollided2_end)==0
+        tree_end(size_tree_end+1).parent = min_dist_idx_end;
+        tree_end(size_tree_end+1).coord = q_new;
+        num_added_tree = num_added_tree + 1;
+        size_tree_end = size(tree_end, 2);
+    end
+    
+    %%%%%%%
+        
     %For live tree plot
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    branch = [tree(min_dist_idx).coord; tree(size_tree+1).coord];
+    branch = [tree(min_dist_idx).coord; tree(size_tree).coord];
     plot3(start(1), start(2), start(3), '.', 'MarkerSize',50, 'color', '#B80F0A')
     hold on
     drawnow
     plot3(goal(1), goal(2), goal(3),'.', 'MarkerSize',50, 'color', '#B80F0A')
     plot3(branch(:,1), branch(:,2), branch(:,3) ,'o-', 'color', '#B80F0A')
+    
+    branch_end = [tree_end(min_dist_idx_end).coord; tree_end(size_tree_end).coord];
+    plot3(branch_end(:,1), branch_end(:,2), branch_end(:,3) ,'o-', 'color', '#B80F0A')
+    
     
     
     % Plot view configuration
@@ -122,23 +183,30 @@ while reachedGoal == 0
     
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    
-    reachedGoal = checkReachedGoal(q_new, goal, epsilon);
-    if reachedGoal
-        path = [goal];
+    %reachedGoal = checkReachedGoal(q_new, goal, epsilon);
+    if num_added_tree == 2
+        path_from_start = [];
+        path_from_goal = []; 
+        
+        %path = [goal];
         tree_node_num = size_tree;
         while(tree_node_num ~= -1)
-            path = [tree(tree_node_num).coord; path];
+            path_from_start = [tree(tree_node_num).coord; path_from_start];
             tree_node_num = tree(tree_node_num).parent;
         end
-        
-        %For live tree plot
-        %%%%%%%%%%%%%%%%%%%%%%
-        plot3(start(1), start(2), start(3), '.', 'MarkerSize',50, 'color', '#2E8B57')
-         plot3(goal(1), goal(2), goal(3),'.', 'MarkerSize',50, 'color', '#2E8B57')
-        plot3(path(:,1), path(:,2), path(:,3),'o-', 'LineWidth', 3,'color','#2E8B57')
-        %%%%%%%%%%%%%%%%%%%%%%%%
+        first = 0;
+        tree_node_num_end = size_tree_end;
+        while(tree_node_num_end ~= -1)
+            if first == 0
+                first = first + 1;
+                tree_node_num_end = tree_end(tree_node_num_end).parent;
+                continue
+            end
+            path_from_goal = [path_from_goal; tree_end(tree_node_num_end).coord];
+            tree_node_num_end = tree_end(tree_node_num_end).parent;
+        end
+       
+        path = [path_from_start; path_from_goal];
         break
     end
     
@@ -186,7 +254,7 @@ end
         % distance between 2 points 
         dist = norm(p2-p1); 
 %         normalized_dist = dist / sqrt(numel(p2));
-        normalized_dist = norm(robot.lowerLim-robot.upperLim);        
+        normalized_dist = norm(start-goal);        
         ratio = dist / normalized_dist;
         num_points = round(alpha * ratio);
         if(num_points==0)
