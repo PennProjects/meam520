43664a6c9d85495630f75ee2fcba36c301f18660
Jal Panchal <jlpnchl@gmail.com>
2020-11-05 16:28:27 -0500

    circle test

--- matlab/Lab4/test_IK_paths.m
+++ matlab/Lab4/test_IK_paths.m
@@ -15,27 +15,39 @@ lowerLim = [-1.4000 -1.2000 -1.8000 -1.9000 -2 -15];
 upperLim = [1.4000 1.4000 1.7000 1.7000 1.5000 30];
 
 
-q = [0 0 -1 0 0 0];
+q = [0 0 0 0 0 0];
 dq = [0 0 0 0 0 0]';
 joint = 6;
-i = 0;
-while (i < 300 & q>lowerLim  & q< upperLim)
-    
-q = q+dq'
 
+size = 300;
+
+th = 0:2*pi/size:2*pi+0.1;
+i = 1;
+while (i < size          & q>lowerLim  & q< upperLim)
+   
 
 [jointPositions,T0e] = calculateFK(q);
 
-v = [0 0 0.5]';
-omega = [0 0 0]';
+%straight line
+% v = [0 0.3 0]';
+% omega = [0 0 0]';
 
-dq = jal_IK_velocity(q,v,omega,joint)
+%circle
+th = 0:2*pi/300:2*pi;
+r = 1;
+v = [0 r*cos(th(i)) r*sin(th(i))]'
+omega = [0 0 0]'
+
+dq = jal_IK_velocity(q,v,omega,joint);
 
 
 
 
 % plot dummy robot at zero confoguration
 plot3(jointPositions(:,1),jointPositions(:,2), jointPositions(:,3),'d-','Color','#000000', 'LineWidth', 5)
+plot3(jointPositions(6,1),jointPositions(6,2), jointPositions(6,3),'d-','Color','r', 'LineWidth', 5)
+hold on
+
 
 
  % Plot view configuration
@@ -45,16 +57,17 @@ xlabel('Xo', 'FontSize', 20, 'FontWeight', 'bold')
 ylabel('Yo', 'FontSize', 20, 'FontWeight', 'bold')
 zlabel('Zo', 'FontSize', 20, 'FontWeight', 'bold')  
 
-xlim([-10, 300])
-ylim([-10 30])
-zlim([-300, 500])
+xlim([-100, 500])
+ylim([-100 500])
+zlim([-100, 500])
 % pause(0.01)
 grid on
 % addpoints(h,i, i+1, i+2);
-drawnow limitrate
+drawnow 
 
 % hold on
 
-i = i+1;    
+i = i+1; 
+q = q+dq'
 end 
 
