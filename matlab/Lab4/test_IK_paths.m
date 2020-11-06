508b4d99ea53ae91fbce91e31d7246d47362bb61
Jal Panchal <jlpnchl@gmail.com>
2020-11-05 23:26:31 -0500

    hold on for traj

--- matlab/Lab4/test_IK_paths.m
+++ matlab/Lab4/test_IK_paths.m
@@ -19,20 +19,25 @@ q = [0 0 0 0 0 0];
 dq = [0 0 0 0 0 0]';
 joint = 6;
 
+
+a1 = animatedline('Color',[0 .7 .7]);
+
 size = 300;
+trajectory = [];
 
 th = 0:2*pi/size:2*pi+0.1;
 i = 1;
-while (i < size          & q>lowerLim  & q< upperLim)
+while (i < size & q>lowerLim  & q< upperLim)
    
 
 [jointPositions,T0e] = calculateFK(q);
 
-%straight line
-% v = [0 0.3 0]';
+% % straight line
+% v = [0 1 0]';
 % omega = [0 0 0]';
 
-%circle
+
+% %circle
 th = 0:2*pi/300:2*pi;
 r = 1;
 v = [0 r*cos(th(i)) r*sin(th(i))]'
@@ -44,9 +49,17 @@ dq = jal_IK_velocity(q,v,omega,joint);
 
 
 % plot dummy robot at zero confoguration
+
+
+trajectory = [trajectory;jointPositions(6,:)];
+
+
 plot3(jointPositions(:,1),jointPositions(:,2), jointPositions(:,3),'d-','Color','#000000', 'LineWidth', 5)
-plot3(jointPositions(6,1),jointPositions(6,2), jointPositions(6,3),'d-','Color','r', 'LineWidth', 5)
 hold on
+plot3(trajectory(:,1),trajectory(:,2), trajectory(:,3),'d-','Color','r', 'LineWidth', 2)
+hold off
+
+
 
 
 
@@ -60,14 +73,13 @@ zlabel('Zo', 'FontSize', 20, 'FontWeight', 'bold')
 xlim([-100, 500])
 ylim([-100 500])
 zlim([-100, 500])
-% pause(0.01)
+% pause(0.1)
 grid on
-% addpoints(h,i, i+1, i+2);
 drawnow 
 
-% hold on
 
 i = i+1; 
 q = q+dq'
 end 
+
 
