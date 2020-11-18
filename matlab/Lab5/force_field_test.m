71d94c59c2f212411da1078aa0813c10bbf4a9e5
Jal Panchal <jlpnchl@gmail.com>
2020-11-17 21:34:46 -0500

    updated force to 3d

--- matlab/Lab5/force_field_test.m
+++ matlab/Lab5/force_field_test.m
@@ -2,23 +2,42 @@
 
 clc
 clear
+% 
+% addpath('maps')
+% map = loadmap('map2.txt');
 
-qCurr = [5 5 5 5 5 5];
-qGoal = [8 9 6 8 9 6]; 
+% o = size(map.obstacles, 1)
 
-rho_a  = [2 2 2 2 2 2];
-zeta = [1 1 1 1 1 1];
+qCurr = [0 0 0 0 0 0];
+[joint_position_curr,~] = calculateFK(qCurr)
 
-distance_from_goal = abs(qCurr-qGoal)
+qGoal = [pi/4 -pi/4 0 pi/4 0 0]; 
+[joint_position_goal,~] = calculateFK(qGoal)
+
+rho_a  = 5*ones(6,1)
+% obd
+zeta = [1;1;1;1;1;1]*ones(1,3)
+
+
+
+% (joint_position_curr(:,1)-joint_position_goal(:,1)).^2
+a = (joint_position_curr(:,1)-joint_position_goal(:,1)).^2 ;
+b = (joint_position_curr(:,2)-joint_position_goal(:,2)).^2 ;
+c = (joint_position_curr(:,3)-joint_position_goal(:,3)).^2;
+
+distance_from_goal = sqrt(a + b + c)
 
 f_att = [];
 
 
 
 % if(distance_from_goal <= rho_a)
-    x = distance_from_goal <= rho_a
-    f_att(x) = -zeta(x).*(qCurr(x)-qGoal(x))
-% else
+%     
     y = distance_from_goal > rho_a
-    f_att(y)= -rho_a(y).*zeta(y).*((qCurr(y)-qGoal(y))/norm(qCurr(y)-qGoal(y)))
-% end
+ for i= 1:6
+     x = distance_from_goal <= rho_a
+    f_att(x,:) = -zeta(x).*(joint_position_curr(x,:)-joint_position_goal(x,:))
+    
+    y = distance_from_goal > rho_a
+    f_att(y,:)= -rho_a(y).*zeta(y).*((joint_position_curr(y,:)-joint_position_goal(y,:))/norm(joint_position_curr(y,:)-joint_position_goal(y,:)))
+ end
